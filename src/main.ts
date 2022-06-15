eff4d037a8f0402c6c0845627b86e3181e2e5728
matucasal <matias.casal@yahoo.com.ar>
2022-06-15 18:04:55 -0300

    agrego validation pipes

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  //Config para las validaciones
+  app.useGlobalPipes(new ValidationPipe());
 
   //Config del swagger
   const config = new DocumentBuilder()
